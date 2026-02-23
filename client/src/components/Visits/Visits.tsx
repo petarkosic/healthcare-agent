9839f55350b26b38eae077fe4fbee51ca27c5c27
petarkosic <avt3914@gmail.com>
2026-02-23 23:01:42 +0100

    fix: minor css fixes

--- client/src/components/Visits/Visits.tsx
+++ client/src/components/Visits/Visits.tsx
@@ -1,5 +1,5 @@
 import type { PatientFullResponse } from '../../types/types';
-// import './Visits.css';
+import './Visits.css';
 
 type VisitsProps = {
 	data: PatientFullResponse;
@@ -9,36 +9,38 @@ export const Visits = ({ data }: VisitsProps) => {
 	return (
 		<div className='card card-visits full-width'>
 			<h3>Visit History</h3>
-			<table className='data-table'>
-				<thead>
-					<tr>
-						<th>Date</th>
-						<th>Type</th>
-						<th>Doctor</th>
-						<th>Chief Complaint</th>
-						<th>Status</th>
-					</tr>
-				</thead>
-				<tbody>
-					{data.visits.map((visit) => (
-						<tr key={visit.visit_id}>
-							<td>{new Date(visit.visit_date).toLocaleString()}</td>
-							<td style={{ textTransform: 'capitalize' }}>
-								{visit?.visit_type?.replace('_', ' ')}
-							</td>
-							<td>
-								{visit.doctor_first_name} {visit.doctor_last_name}
-							</td>
-							<td>{visit.chief_complaint || '-'}</td>
-							<td>
-								<span className={`status-badge status-${visit.status}`}>
-									{visit.status}
-								</span>
-							</td>
+			<div className='visits-timeline'>
+				<table className='data-table'>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>Type</th>
+							<th>Doctor</th>
+							<th>Chief Complaint</th>
+							<th>Status</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{data.visits.map((visit) => (
+							<tr key={visit.visit_id}>
+								<td>{new Date(visit.visit_date).toLocaleString()}</td>
+								<td style={{ textTransform: 'capitalize' }}>
+									{visit?.visit_type?.replace('_', ' ')}
+								</td>
+								<td>
+									{visit.doctor_first_name} {visit.doctor_last_name}
+								</td>
+								<td>{visit.chief_complaint || '-'}</td>
+								<td>
+									<span className={`status-badge status-${visit.status}`}>
+										{visit.status}
+									</span>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 };

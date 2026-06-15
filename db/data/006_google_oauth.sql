6fefc0299db5efdcbee9d57b77d0cf2d4e9328da
petarkosic <avt3914@gmail.com>
2026-06-15 23:28:07 +0200

    remove: raw sql files

--- db/data/006_google_oauth.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE doctors ADD COLUMN IF NOT EXISTS google_access_token TEXT;
-ALTER TABLE doctors ADD COLUMN IF NOT EXISTS google_refresh_token TEXT;
-ALTER TABLE doctors ADD COLUMN IF NOT EXISTS google_token_expiry TIMESTAMP;
